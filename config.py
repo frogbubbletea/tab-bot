e41fca38729bc22c604f211d318f9a7454320de4
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2023-03-12 03:12:55 +0800

    change embed color

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import datetime
 
 # Embed colors
-color_success = 0xa6e3a1	
+color_success = 0x89dceb	
 color_failure = 0xf38ba8
 color_warning = 0xf9e2af
-color_info = 0x94e2d5
+color_info = 0x89dceb
