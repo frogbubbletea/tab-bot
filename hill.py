a4e91138b082beb75df07fbaa54097357c94d2ed
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2023-04-16 04:39:47 +0800

    unify views for all commands, add source button

--- hill.py
+++ hill.py
@@ -51,17 +51,23 @@ async def on_guild_join(guild):
     )
 
 # Slash commands start
-# "quota" command
-# Compose page flip buttons
+# Compose page flip buttons for all commands
 class QuotaPage(discord.ui.View):
-    def __init__(self, *, timeout=180, course_code="", page=0):
+    def __init__(self, *, timeout=180, mode="q", course_code="", page=0):
         super().__init__(timeout=timeout)
+        self.mode = mode
         self.course_code = course_code
         self.page = page
 
     @discord.ui.button(label="Previous page", style=discord.ButtonStyle.gray, emoji="⬅️")
     async def previous_button(self,interaction:discord.Interaction, button:discord.ui.Button):
-        embed_quota_pageflip = get_quota.compose_message(self.course_code, self.page - 1)
+        if self.mode == "q":
+            embed_quota_pageflip = get_quota.compose_message(self.course_code, self.page - 1)
+        elif self.mode == "s":
+            embed_quota_pageflip = get_quota.compose_sections(self.course_code, self.page - 1)
+        # No pages for info command
+        # elif self.mode == "i":
+        #     button.disabled = True
 
         if embed_quota_pageflip == "p0":
             await interaction.response.send_message("🚫 You're already at the first page!", ephemeral=True)
@@ -75,7 +81,13 @@ class QuotaPage(discord.ui.View):
 
     @discord.ui.button(label="Next page", style=discord.ButtonStyle.gray, emoji="➡️")
     async def next_button(self,interaction:discord.Interaction, button:discord.ui.Button):
-        embed_quota_pageflip = get_quota.compose_message(self.course_code, self.page + 1)
+        if self.mode == "q":
+            embed_quota_pageflip = get_quota.compose_message(self.course_code, self.page + 1)
+        elif self.mode == "s":
+            embed_quota_pageflip = get_quota.compose_sections(self.course_code, self.page + 1)
+        # No pages for info command
+        # elif self.mode == "i":
+        #     button.disabled = True
 
         if embed_quota_pageflip == "pmax":
             await interaction.response.send_message("🚫 You're already at the last page!", ephemeral=True)
@@ -87,7 +99,12 @@ class QuotaPage(discord.ui.View):
             else:
                 self.page += 1
 
-# Actual command
+# Get source URL on original UST course quota website for "Source" button
+def get_source_url(course_code):
+    source_url = f"https://w5.ab.ust.hk/wcq/cgi-bin/{get_quota.semester_code}/subject/{course_code[0: 4]}#{course_code}"
+    return source_url
+
+# "quota" command
 # Lists quotas of all sections of a course
 @bot.tree.command(description="Get quotas for a course!", guilds=bot.guilds)
 async def quota(interaction: discord.Interaction, course_code: str) -> None:
@@ -107,7 +124,9 @@ async def quota(interaction: discord.Interaction, course_code: str) -> None:
         await interaction.edit_original_response(content="⚠️ This course has no sections!")
     else:
         try:
-            await interaction.edit_original_response(embed=embed_quota, view=QuotaPage(course_code=course_code))
+            view = QuotaPage(mode="q", course_code=course_code)
+            view.add_item(discord.ui.Button(label="Source", style=discord.ButtonStyle.link, url=get_source_url(course_code)))
+            await interaction.edit_original_response(embed=embed_quota, view=view)
         except:  # Deprecated: large numbers of sections should be displayed correctly with pagination
             await interaction.edit_original_response(content="⚠️ This course has too many sections!\nDue to a Discord limitation, the sections field is limited to 1024 characters long.\nThis translates to around 15 sections.")
 
@@ -117,6 +136,7 @@ async def quota(interaction: discord.Interaction, course_code: str) -> None:
 async def info(interaction: discord.Interaction, course_code: str) -> None:
     await interaction.response.defer(thinking=True)
 
+    course_code = course_code.replace(" ", "").upper()
     embed_info = get_quota.compose_info(course_code.replace(" ", "").upper())
 
     # Error: Course data unavailable
@@ -127,47 +147,14 @@ async def info(interaction: discord.Interaction, course_code: str) -> None:
         await interaction.edit_original_response(content="⚠️ Check your course code!")
     else:
         try:
-            await interaction.edit_original_response(embed=embed_info)
+            view = QuotaPage(mode="i", course_code=course_code)
+            view.clear_items()
+            view.add_item(discord.ui.Button(label="Source", style=discord.ButtonStyle.link, url=get_source_url(course_code)))
+            await interaction.edit_original_response(embed=embed_info, view=view)
         except:  # Deprecated: long course info text should be displayed correctly by splitting into multiple fields
             await interaction.edit_original_response(content="⚠️ Course info too long!\nDue to a Discord limitation, course info is limited to 1024 characters long.")
 
 # "sections" command
-# Compose page flip buttons
-class SectionPage(discord.ui.View):
-    def __init__(self, *, timeout=180, course_code="", page=0):
-        super().__init__(timeout=timeout)
-        self.course_code = course_code
-        self.page = page
-
-    @discord.ui.button(label="Previous page", style=discord.ButtonStyle.gray, emoji="⬅️")
-    async def previous_button(self,interaction:discord.Interaction, button:discord.ui.Button):
-        embed_sections_pageflip = get_quota.compose_sections(self.course_code, self.page - 1)
-
-        if embed_sections_pageflip == "p0":
-            await interaction.response.send_message("🚫 You're already at the first page!", ephemeral=True)
-        else:
-            try:
-                await interaction.response.edit_message(embed=embed_sections_pageflip, view=self)
-            except:  # Highly unlikely to happen
-                await interaction.response.send_message(f"⚠️ Error: {embed_sections_pageflip}!")
-            else:
-                self.page -= 1
-
-    @discord.ui.button(label="Next page", style=discord.ButtonStyle.gray, emoji="➡️")
-    async def next_button(self,interaction:discord.Interaction, button:discord.ui.Button):
-        embed_sections_pageflip = get_quota.compose_sections(self.course_code, self.page + 1)
-
-        if embed_sections_pageflip == "pmax":
-            await interaction.response.send_message("🚫 You're already at the last page!", ephemeral=True)
-        else:
-            try:
-                await interaction.response.edit_message(embed=embed_sections_pageflip, view=self)
-            except:  # Highly unlikely to happen
-                await interaction.response.send_message(f"⚠️ Error: {embed_sections_pageflip}!")
-            else:
-                self.page += 1
-
-# Actual command
 # Lists sections of a course and their times, venues and instructors
 @bot.tree.command(description="Get sections of a course!", guilds=bot.guilds)
 async def sections(interaction: discord.Interaction, course_code: str) -> None:
@@ -187,7 +174,9 @@ async def sections(interaction: discord.Interaction, course_code: str) -> None:
         await interaction.edit_original_response(content="⚠️ This course has no sections!")
     else:
         try:
-            await interaction.edit_original_response(embed=embed_sections, view=SectionPage(course_code=course_code))
+            view = QuotaPage(mode="s", course_code=course_code)
+            view.add_item(discord.ui.Button(label="Source", style=discord.ButtonStyle.link, url=get_source_url(course_code)))
+            await interaction.edit_original_response(embed=embed_sections, view=view)
         except:  # Deprecated: large numbers of sections should be displayed correctly with pagination
             await interaction.edit_original_response(content="⚠️ This course has too many sections!\nDue to a Discord limitation, courses with more than 25 sections cannot be displayed.")
 # Slash commands end
