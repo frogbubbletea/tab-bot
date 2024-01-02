2ee8cdc8c17531011f7faef87e60fcf7b66be8a3
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2024-01-02 19:07:51 +0800

    refactor(tab): use .env to get admin and test server ID

--- bot.py
+++ bot.py
@@ -30,6 +30,10 @@ os.chdir(dname)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Load admin's ID and test server ID
+admin_id = int(os.getenv('ADMIN_ID'))
+test_server_id = int(os.getenv('TEST_SERVER_ID'))
+
 # Uncomment when running on replit (1/2)
 # from keep_alive import keep_alive
 
@@ -38,39 +42,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix="-", intents=intents, activity=discord.Game(name="Doki Doki Literature Club!"), help_command=None)
-
-# Helper function to check if course/section/quota changed
-# async def check_diffs():
-#     # Open quotas files
-#     new_quotas = quotas_operations.open_quotas()
-#     old_quotas = open('quotas_old.json', encoding='utf-8')
-#     try:
-#         old_quotas = json.load(old_quotas)
-#     except:
-#         return
-
-#     # No comparison if current quotas file or last quotas file is corrupted
-#     if (not quotas_operations.check_quotas_validity) or (old_quotas == {}):
-#         return
-    
-#     for key, value in new_quotas.items():
-#         # Skip 'time' entry
-#         if key == 'time':
-#             continue
-#         # New course
-#         if key not in old_quotas:
-#             await channels.get(key[0: 4], channels['other']).send(f"🥑 New course!\n{value.get('title', 'Error')}\n{len(value['sections'])} sections")
-#         else:
-#             for key2, value2 in value['sections'].items():
-#                 # New section
-#                 if key2 not in old_quotas[key]['sections']:
-#                     quota_new = value2[4].split("\n", 1)[0]
-#                     await channels.get(key[0: 4], channels['other']).send(f"🍅 New section!\n{value.get('title', 'Error')}: {key2}\nQuota: {quota_new}")
-#                 # Quota change
-#                 elif value2[4].split("\n", 1)[0] != old_quotas[key]['sections'][key2][4].split("\n", 1)[0]:  # DEBUG: Compare waitlist instead of quota 
-#                     quota_old = old_quotas[key]['sections'][key2][4].split("\n", 1)[0]
-#                     quota_new = value2[4].split("\n", 1)[0]
-#                     await channels.get(key[0: 4], channels['other']).send(f"🍋 Quota changed!\n{value.get('title', 'Error')}: {key2}\n{quota_old} -> {quota_new}")
 
 # Update quotas every 1.5 minutes
 # quotas on the website is updated at 03, 18, 33, 48 minutes
@@ -130,36 +101,20 @@ async def on_guild_join(guild):
     )
 
 # Commands have been moved to Hill!
-# Slash commands start
-# "test" command
-# Tests bot status
-# @bot.tree.command(description="Tests bot status", guilds=bot.guilds)
-# async def test(interaction: discord.Interaction) -> None:
-#     await interaction.response.send_message("meow")
-
-# @bot.tree.command(description="Get quota information for a course!", guilds=bot.guilds)
-# async def quota(interaction: discord.Interaction, course_code: str) -> None:
-#     await interaction.response.defer(thinking=True)
-
-#     embed_quota = get_quota.compose_message(course_code.replace(" ", "").upper())
-    
-#     if embed_quota == "key":
-#         await interaction.edit_original_response(content="⚠️ Check your course code!")
-#     else:
-#         try:
-#             await interaction.edit_original_response(embed=embed_quota)
-#         except:
-#             await interaction.edit_original_response(content="⚠️ This course has too many sections!\nDue to a Discord limitation, the sections field is limited to 1024 characters long.\nThis translates to around 15 sections.")
-# Slash commands end
 
 # Text commands start
 # "sync" command
 # Syncs command tree with Discord
-@commands.guild_only()
 @bot.command()
 async def sync(ctx):
-    if ctx.author.id == 740098404688068641:
+    if ctx.author.id == admin_id:  # Owner's ID
+        # Sync global commands
         await bot.tree.sync()
+
+        # Sync guild specific commands
+        for guild in bot.guilds:
+            await bot.tree.sync(guild=guild)
+
         await ctx.send("👍 Commands synced!")
     else:
         await ctx.send("🚫 This command can only be ran by the admin!")
