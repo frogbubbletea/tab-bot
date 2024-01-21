b8ff899b1046379ee4bb161d064d165944670dc3
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2024-01-22 00:29:30 +0800

    build: bump version to 2.9

--- get_quota.py
+++ get_quota.py
@@ -16,7 +16,7 @@ import config
 import subject_channels
 
 # Bots version
-bot_version = 2.8
+bot_version = 2.9
 
 # Change working directory to wherever this is in
 abspath = os.path.abspath(__file__)
