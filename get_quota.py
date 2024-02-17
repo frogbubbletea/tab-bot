172a282418816851aacfed7838d461eef1110aac
succsuccsucc <jadenmills@CharlessMacBookPro>
2024-02-17 22:52:44 +0800

    build: bump version to 3.1.1

--- get_quota.py
+++ get_quota.py
@@ -18,7 +18,7 @@ import config
 import subject_channels
 
 # Bots version
-bot_version = "3.1"
+bot_version = "3.1.1"
 
 # Change working directory to wherever this is in
 abspath = os.path.abspath(__file__)
