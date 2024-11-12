ada16490d5aa3918122c1bad5d5c32d73a0abe3b
frogbubbletea <109941602+frogbubbletea@users.noreply.github.com>
2024-11-12 17:46:10 +0800

    feat: update to 2024-25 Winter

--- get_quota.py
+++ get_quota.py
@@ -32,7 +32,7 @@ os.chdir(dname)
 # Winter: 20
 # Spring: 30
 # Summer: 40
-semester_code = 2410  # 24-25 Fall
+semester_code = 2420  # 24-25 Winter
 
 # Timezone of HKUST
 hkust_time_zone = "Asia/Hong_Kong"
