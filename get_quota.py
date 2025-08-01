d454ee33de8217ec2e8c66035573e16e3454ff92
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-08-01 21:46:27 +0800

    chore: switch to 2025-26 fall semester

--- get_quota.py
+++ get_quota.py
@@ -32,7 +32,7 @@ os.chdir(dname)
 # Winter: 20
 # Spring: 30
 # Summer: 40
-semester_code = 2440  # 24-25 Summer
+semester_code = 2510  # 25-26 Fall
 
 # Timezone of HKUST
 hkust_time_zone = "Asia/Hong_Kong"
